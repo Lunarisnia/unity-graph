785a2782764e8f19c01f9366bc15404ba976ac82
Rio Arswendo Rachmad <r.arswendo.r@gmail.com>
2025-01-29 21:39:00 +0700

    Extending to 3Dimension

--- Assets/Scripts/FunctionLibrary.cs
+++ Assets/Scripts/FunctionLibrary.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public enum FunctionName
 {
+    None,
     Wave,
     MultiWave,
     Ripple
@@ -9,33 +10,51 @@ public enum FunctionName
 
 public static class FunctionLibrary
 {
-    public delegate float Function(float u, float v, float t);
+    public delegate Vector3 Function(float u, float v, float t);
 
 
-    private static readonly Function[] _functions = { SineWave, MultiWave, Ripple };
+    private static readonly Function[] _functions = { None, SineWave, MultiWave, Ripple };
 
     public static Function GetFunction(FunctionName functionName)
     {
         return _functions[(int)functionName];
     }
 
-    private static float SineWave(float x, float z, float t)
+    private static Vector3 None(float u, float v, float t)
     {
-        return Mathf.Sin(Mathf.PI * (x + z + t));
+        return new Vector3(u, 0.0f, v);
     }
 
-    private static float MultiWave(float x, float z, float t)
+    private static Vector3 SineWave(float u, float v, float t)
     {
-        var y = SineWave(x, z, t * 0.5f);
-        y += 0.5f * Mathf.Sin(2.0f * Mathf.PI * (z + t));
-        return y * (2f / 3f);
+        Vector3 p;
+        p.x = u;
+        p.y = Mathf.Sin(Mathf.PI * (u + v + t));
+        p.z = v;
+        return p;
     }
 
-    private static float Ripple(float x, float z, float t)
+    private static Vector3 MultiWave(float u, float v, float t)
+    {
+        var y = SineWave(u, v, t * 0.5f).y;
+        y += 0.5f * Mathf.Sin(2.0f * Mathf.PI * (v + t));
+
+        Vector3 p;
+        p.x = u;
+        p.y = y * (2f / 3f);
+        p.z = v;
+        return p;
+    }
+
+    private static Vector3 Ripple(float u, float v, float t)
     {
         // Distance from center (0, 0)
-        var d = Mathf.Sqrt(x * x + z * z);
+        var d = Mathf.Sqrt(u * u + v * v);
         var y = Mathf.Sin(Mathf.PI * (4f * d - t));
-        return y / (1f + 10f * d);
+        Vector3 p;
+        p.x = u;
+        p.y = y / (1f + 10f * d);
+        p.z = v;
+        return p;
     }
 }
--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -14,22 +14,12 @@ public class Graph : MonoBehaviour
     private void Start()
     {
         points = new Transform[resolution * resolution];
-        var position = Vector3.zero;
         var step = 2f / resolution;
         var scale = Vector3.one * step;
-        for (int i = 0, x = 0, z = 0; i < points.Length; x++, i++)
+        for (var i = 0; i < points.Length; i++)
         {
-            if (x == resolution)
-            {
-                x = 0;
-                z++;
-            }
-
             var point = Instantiate(PointPrefab, transform);
-            position.x = (x + 0.5f) * step - 1.0f;
-            position.z = (z + 0.5f) * step - 1.0f;
 
-            point.localPosition = position;
             point.localScale = scale;
             points[i] = point;
         }
@@ -38,16 +28,22 @@ public class Graph : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        foreach (var point in points)
+        var step = 2f / resolution;
+        for (int i = 0, x = 0, z = 0; i < points.Length; x++, i++)
         {
-            var position = point.localPosition;
-            position.y = CalculateWave(functionName, position.x, position.z, Time.time);
+            if (x == resolution)
+            {
+                x = 0;
+                z++;
+            }
 
-            point.localPosition = position;
+            var u = (x + 0.5f) * step - 1.0f;
+            var v = (z + 0.5f) * step - 1.0f;
+            points[i].localPosition = CalculateWave(functionName, u, v, Time.time);
         }
     }
 
-    private float CalculateWave(FunctionName functionName, float x, float z, float t)
+    private Vector3 CalculateWave(FunctionName functionName, float x, float z, float t)
     {
         return FunctionLibrary.GetFunction(functionName)(x, z, t);
     }
